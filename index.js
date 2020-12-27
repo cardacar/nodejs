eefe0af5ae0d2baed08484236b96fff6972e0e9b
Carlos Quiros <carlosquiros12@gmail.com>
2020-12-26 20:44:12 -0500

    express

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req,res)=>{
+    res.send('<h1>Hola mundo con nodejs y express</h1>');
+});
+
 server.listen(3000, ()=>{
     console.log('Server on port 3000')
 });
