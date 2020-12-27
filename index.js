770db95201af308f4716d6d0df3108727be77576
Carlos Quiros <carlosquiros12@gmail.com>
2020-12-26 19:41:24 -0500

    Creando un servidor con express

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Creando servidor http con node
-const http = require('http');
+/* const http = require('http');
  
 const handleServer = function(req, res){
     res.writeHead(200, {'Content-type': ' text/html'});
@@ -11,6 +11,15 @@ const server = http.createServer(handleServer);
 
 server.listen(3000, function(){
     console.log('Servidor en el puerto 3000');
+}); */
+
+//Creando server con nodejs
+const express = require('express');
+
+const server = express();
+
+server.listen(3000, ()=>{
+    console.log('Server on port 3000')
 });
 
 
