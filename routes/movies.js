9486bbe4b0291722e31d3a852f1431217646be7f
Carlos Quiros <carlosquiros12@gmail.com>
2020-12-29 19:46:58 -0500

    GET y POST en una api

--- routes/movies.js
+++ routes/movies.js
@@ -7,4 +7,18 @@ router.get('/', (req, res)=>{
     res.json(movies);
 });
 
+router.post('/', (req, res)=>{
+    const {title, director, year, rating} = req.body;
+    if(title && director && year && rating){
+        const id = movies.length+1;
+        const newMovie = {...req.body, id};
+        console.log(newMovie);
+        movies.push(newMovie);
+        res.json(movies);
+    }else{
+        res.status(500).json({error:'There was an error'});
+    }
+});
+
+
 module.exports = router;
